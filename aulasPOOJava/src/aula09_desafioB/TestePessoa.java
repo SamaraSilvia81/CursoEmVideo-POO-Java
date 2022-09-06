bba728985602fdb553cb77348cca5cfe37e75810
Samara Silvia <100232025+SamaraSilvia81@users.noreply.github.com>
2022-09-06 18:52:58 -0300

    Update TestePessoa.java

--- aulasPOOJava/src/aula09_desafioB/TestePessoa.java
+++ aulasPOOJava/src/aula09_desafioB/TestePessoa.java
@@ -4,17 +4,22 @@ public class TestePessoa {
 	
 	public static void main(String[] args) {
 		
+			
 		Pessoa [] p = new Pessoa[2];
 		Livro [] book = new Livro[3];
 		
-	
-		//System.out.println("Página atual: " + book.getPagAtual());
-		//book.avancarPag();
-		//book.voltarPag();
-		//System.out.println("Página atual: " + book.getPagAtual());
+		p[0] = new Pessoa("Samara", 22,"F");
+		p[1] = new Pessoa("Thomas",42,"M");
 		
-		p[0] = 
-		p[1] =
+		book[0] = new Livro("A Cabana","William P.Young",240,p[0]);
+		book[1] = new Livro("A Travessia","William P.Young",340,p[1]);
+		book[2] = new Livro("Eva","William P.Young",540,p[0]);
+		
+		book[0].abrir();
+		book[0].folhear(400);
+		System.out.println(book[0].detalhes());
+		
+		
 	
 	}
 
